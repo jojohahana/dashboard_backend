6df220595eee236d7bc2d75eeeb201789aed0abd
jojohahana <yohana.chriezt@gmail.com>
2025-01-23 14:41:55 +0700

    clean documentation

--- app/Http/Controllers/Api/DeyeController.php
+++ app/Http/Controllers/Api/DeyeController.php
@@ -81,7 +81,6 @@ class DeyeController extends Controller
 
     public function getSumDeviceHistory(Request $request)
     {
-        // Define the list of device serial numbers
         $deviceSnList = [
             "2404164849",
             "2404164857",
@@ -101,12 +100,12 @@ class DeyeController extends Controller
         foreach ($deviceSnList as $deviceSn) {
             // Prepare the request to the external API
             $response = Http::withHeaders([
-                'Authorization' => 'Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOlsib2F1dGgyLXJlc291cmNlIl0sInVzZXJfbmFtZSI6IjBfcGx0cy5vamkuMjAyNEBnbWFpbC5jb21fMiIsInNjb3BlIjpbImFsbCJdLCJkZXRhaWwiOnsib3JnYW5pemF0aW9uSWQiOjAsInRvcEdyb3VwSWQiOm51bGwsImdyb3VwSWQiOm51bGwsInJvbGVJZCI6LTEsInVzZXJJZCI6MTI5OTczMjIsInZlcnNpb24iOjEwMDAsImlkZW50aWZpZXIiOiJwbHRzLm9qaS4yMDI0QGdtYWlsLmNvbSIsImlkZW50aXR5VHlwZSI6MiwibWRjIjoidWMiLCJhcHBJZCI6IjIwMjUwMTEwNjk3NTAwMiIsIm1mYVN0YXR1cyI6bnVsbCwidGVuYW50IjoiRGV5ZSJ9LCJleHAiOjE3NDE2Nzc4NDUsIm1kYyI6InVjIiwiYXV0aG9yaXRpZXMiOlsiYWxsIl0sImp0aSI6Ijk3NTRlZGIwLWQxMTYtNGZkMS1hZThhLTMyZmZkODM0MDcxNyIsImNsaWVudF9pZCI6InRlc3QifQ.HzLodCpaEkKLyyge1DmylQ2sS9_qSoEwRDjHd51RidJf_2Osh2pv-Vej1hAKakPnuZpqFN7Twnkr_3SlIPqIJ7wfhMRusXedai2wwcqQp4pylhytI2LJGW5X4arvB5oU0rHkqGEE27cEyEoqHFftHV6_hh5Domkr_L5Ry-jQyK90vKKi6LywwoYhnIiqqo477Ylj2W7PzIdF6MwEEKY004zxDi3nnqC7dodMKtWB3vSFWCeFKkqwcxes2HNJgc9BEqdX59NWs0WWeqAb8TbToKRTxfMCGkY-ROB7kyQHKclTHnrDvCu4WkswDO-Lppca4rQq8DYcA-hras7gaBbpxQ', // Replace with your actual token
+                'Authorization' => 'Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOlsib2F1dGgyLXJlc291cmNlIl0sInVzZXJfbmFtZSI6IjBfcGx0cy5vamkuMjAyNEBnbWFpbC5jb21fMiIsInNjb3BlIjpbImFsbCJdLCJkZXRhaWwiOnsib3JnYW5pemF0aW9uSWQiOjAsInRvcEdyb3VwSWQiOm51bGwsImdyb3VwSWQiOm51bGwsInJvbGVJZCI6LTEsInVzZXJJZCI6MTI5OTczMjIsInZlcnNpb24iOjEwMDAsImlkZW50aWZpZXIiOiJwbHRzLm9qaS4yMDI0QGdtYWlsLmNvbSIsImlkZW50aXR5VHlwZSI6MiwibWRjIjoidWMiLCJhcHBJZCI6IjIwMjUwMTEwNjk3NTAwMiIsIm1mYVN0YXR1cyI6bnVsbCwidGVuYW50IjoiRGV5ZSJ9LCJleHAiOjE3NDE2Nzc4NDUsIm1kYyI6InVjIiwiYXV0aG9yaXRpZXMiOlsiYWxsIl0sImp0aSI6Ijk3NTRlZGIwLWQxMTYtNGZkMS1hZThhLTMyZmZkODM0MDcxNyIsImNsaWVudF9pZCI6InRlc3QifQ.HzLodCpaEkKLyyge1DmylQ2sS9_qSoEwRDjHd51RidJf_2Osh2pv-Vej1hAKakPnuZpqFN7Twnkr_3SlIPqIJ7wfhMRusXedai2wwcqQp4pylhytI2LJGW5X4arvB5oU0rHkqGEE27cEyEoqHFftHV6_hh5Domkr_L5Ry-jQyK90vKKi6LywwoYhnIiqqo477Ylj2W7PzIdF6MwEEKY004zxDi3nnqC7dodMKtWB3vSFWCeFKkqwcxes2HNJgc9BEqdX59NWs0WWeqAb8TbToKRTxfMCGkY-ROB7kyQHKclTHnrDvCu4WkswDO-Lppca4rQq8DYcA-hras7gaBbpxQ',
                 'Content-Type' => 'application/json',
             ])->post('https://eu1-developer.deyecloud.com/v1.0/device/history', [
                 'deviceSn' => $deviceSn,
-                'startAt' => Carbon::now()->format('Y-m-d'), // Use today's date for startAt
-                'endAt' => Carbon::now()->format('Y-m-d'),   // Use today's date for endAt
+                'startAt' => Carbon::now()->format('Y-m-d'),
+                'endAt' => Carbon::now()->format('Y-m-d'),
                 'granularity' => 1,
                 'measurePoints' => ['TotalConsumption'],
             ]);
@@ -133,7 +132,7 @@ class DeyeController extends Controller
         // Return the summary result
         return response()->json([
             'totalConsumption' => $totalConsumption,
-            'time' => $currentEpochTime, // Return the current time as well
+            'time' => $currentEpochTime, // Return the current time as well Epoch format
         ]);
     }
 }
